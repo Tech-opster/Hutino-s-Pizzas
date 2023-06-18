a31f1229f964dfeef3b9224556f645349fd66331
Techopster <111256758+Techopster@users.noreply.github.com>
2023-06-18 20:58:52 -0300

    quase finalizado

--- ProjetoPizza.js
+++ ProjetoPizza.js
@@ -1,3 +1,7 @@
+let modalKey = 0;
+let cart = [];
+let priceCart = 0;
+
 pizzaJson.map((item, index) => {
     let pizzaItem = document.querySelector('.pizzaCard').cloneNode(true);
 
@@ -7,6 +11,8 @@ pizzaJson.map((item, index) => {
     pizzaItem.querySelector('.text p').innerHTML = `R$ ${item.price.toFixed(2)}`;
     pizzaItem.querySelector('.text h1').innerHTML = item.name;
     pizzaItem.querySelector('.descPizza').innerHTML = item.descri;
+
+    document.querySelector('.container').append(pizzaItem);
 
 //ABRIR MODAL
     pizzaItem.querySelector('a').addEventListener('click', (e) => {
@@ -18,6 +24,7 @@ pizzaJson.map((item, index) => {
         }, 1)
 
         let key = e.target.closest('.pizzaCard').getAttribute('data-key');
+        modalKey = key;
 
         const pizzaBigger = document.querySelectorAll('.pizzaBig');
         const imgSrc = pizzaJson[key].img;
@@ -49,12 +56,14 @@ pizzaJson.map((item, index) => {
             descIngre.appendChild(label);
         };
 
+//MODAL INGREDIENTES ADD
         const descAdd = document.querySelector('.descAdd');
         descAdd.innerHTML = '';
-        
+
         for (let i = 0; i < pizzaJson[key].remAdd.length; i++) {
             const checkBox = document.createElement('input');
             checkBox.type = 'checkbox';
+            checkBox.dataset.price = pizzaJson[key].remAdd[i].priceAdd;
 
             const span = document.createElement('span');
             span.className = 'span1'
@@ -70,7 +79,28 @@ pizzaJson.map((item, index) => {
             label.appendChild(spanPrice);
 
             descAdd.appendChild(label);
+
+            checkBox.addEventListener('change', updateTotalPrice);
         };
+
+        updateTotalPrice();
+
+        function updateTotalPrice() {
+            const checkboxes = document.querySelectorAll('.descAdd input[type="checkbox"]');
+            let totalPrice = pizzaJson[key].price;
+        
+            checkboxes.forEach((checkbox) => {
+            if (checkbox.checked) {
+                const price = parseFloat(checkbox.dataset.price);
+                priceCart = totalPrice += price;
+                } else {
+                    priceCart = totalPrice;
+                }
+            });
+        
+            document.querySelector('.totalPrice').innerHTML = `R$ ${totalPrice.toFixed(2)}`;
+        };
+    });
 });
 
 //COMPRAR MODAL
@@ -88,19 +118,6 @@ pizzaJson.map((item, index) => {
         setTimeout(() => {
             document.querySelector('.modalAdd').style.opacity = 1;
         }, 1)
-
-        const checkboxes = document.querySelectorAll('.descIngre input[type="checkbox"]');
-        let selectedIngredients = [];
-        const selectedIngre = pizzaJson[index].remAdd[index];
-        
-
-        for (let i = 0; i < checkboxes.length; i++) {
-            const checkbox = checkboxes[i];
-            if (checkbox.checked) {
-                selectedIngre.removed = true; // marcar o ingrediente como removido
-                checkbox.parentNode.remove(); // remover o label com o checkbox e o texto do ingrediente
-            }
-        };
     });
 
 //FECHAR MODAL
@@ -131,9 +148,107 @@ pizzaJson.map((item, index) => {
         setTimeout(() => {
             document.querySelector('.modalAdd').style.display = 'none';
         }, 300)
+    };
+
+//ADICIONAR AO CARRINHO
+    document.querySelector('.addCart').addEventListener('click', () => {
+    const selectedIngredients = [];
+    const deselectedIngredients = [];
+
+    document.querySelectorAll('.descIngre input[type="checkbox"]:checked').forEach((checkbox) => {
+        const deselectedIngredientName = checkbox.nextElementSibling.textContent;
+        deselectedIngredients.push(deselectedIngredientName);
+    });
+
+    document.querySelectorAll('.descAdd input[type="checkbox"]:checked').forEach((checkbox) => {
+        const ingredientName = checkbox.nextElementSibling.textContent;
+        selectedIngredients.push(ingredientName);
+    });
+
+    let identifier = pizzaJson[modalKey].id+' @ '+deselectedIngredients+selectedIngredients;
+    let key = cart.findIndex((item) => item.identifier == identifier);
+
+    if (key > -1) {
+        cart[key].quantity += 1;
+    } else {
+            cart.push ({
+            identifier,
+            id: pizzaJson[modalKey].id,
+            desIngre: deselectedIngredients,
+            ingre: selectedIngredients,
+            quantity: 1,
+            priceCart
+        });
     }
+    console.log(identifier)
 
-    document.querySelector('.container').append(pizzaItem);
-});
+    updateCart();
+    closeModAdd();
+    });
 
-console.log(pizzaJson[0].remAdd)
+    document.querySelector('.menuOpener').addEventListener('click', () => {
+        if (cart.length > 0) {
+            document.querySelector('aside').style.left = '0vw'
+        };
+    });
+
+    document.querySelector('.menuCloser').addEventListener('click', () => {
+            document.querySelector('aside').style.left = '100vw'
+    });
+
+//ABRIR CARRINHO
+    function updateCart() {
+        document.querySelector('.menuOpener span').innerHTML = cart.length;
+
+        if (cart.length > 0) {
+            document.querySelector('aside').classList.add('show');
+            document.querySelector('.cartAppend').innerHTML = '';
+
+            let subtotal = 0;
+
+            for (let i in cart) {
+                let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+                subtotal += cart[i].priceCart * cart[i].quantity;
+
+                let cartItem = document.querySelector('.totalCart').cloneNode(true);
+
+                cartItem.querySelector('.imgCar img').src = pizzaItem.img;
+                cartItem.querySelector('.pizzaCar').innerHTML = pizzaItem.name;
+                cartItem.querySelector('.qtTotal').innerHTML = cart[i].quantity;
+                
+                if (cart[i].desIngre.length !== 0) {
+                    cartItem.querySelector('.remItem').innerHTML = `Remover: ${cart[i].desIngre}`;
+                } else {
+                    cartItem.querySelector('.remItem').innerHTML = '';
+                }
+
+                if (cart[i].ingre.length !== 0) {
+                    cartItem.querySelector('.addItem').innerHTML = `Adicionar: ${cart[i].ingre}`;
+                } else {
+                    cartItem.querySelector('.addItem').innerHTML = '';
+                }
+                
+                cartItem.querySelector('.qtMenos').addEventListener('click', () => {
+                    if (cart[i].quantity > 1) {
+                        cart[i].quantity--;
+                    } else {
+                        cart.splice(i, 1);
+                    }
+
+                    updateCart();
+                });
+                cartItem.querySelector('.qtMais').addEventListener('click', () => {
+                    cart[i].quantity++;
+
+                    updateCart();
+                });
+
+                document.querySelector('.cartAppend').append(cartItem);
+            }
+
+            document.querySelector('.fullPrice span').innerHTML = `R$ ${subtotal.toFixed(2)}`
+        } else {
+            document.querySelector('aside').classList.remove('show');
+            document.querySelector('aside').style.left = '100vw';
+        }
+    };
--- ProjetoPizzaJSON.js
+++ ProjetoPizzaJSON.js
@@ -6,9 +6,9 @@ let pizzaJson = [
     price: 30.5,
     remAdd: [
       { name: "Mussarela", priceAdd: 2.00}, 
-      { name: "Molho De Tomate", priceAdd: 4.00}, 
-      { name: "Tomate Em Rodelas", priceAdd: 6.00}, 
-      { name: "Orégano", priceAdd: 8.00}
+      { name: " Molho de tomate", priceAdd: 4.00}, 
+      { name: " Tomate em rodelas", priceAdd: 6.00}, 
+      { name: " Orégano", priceAdd: 8.00}
     ],
     descri:
       "Coberta com molho de tomate, queijo tipo mussarela, azeitonas pretas e orégano e massa com fermentação natural.",
@@ -20,11 +20,11 @@ let pizzaJson = [
     img: "../../Mídia/Imagens/pizza2.png",
     price: 32.8,
     remAdd: [
-      { name: "Calabresa Defumada", priceAdd: 3.00},
-      { name: "Cebola", priceAdd: 3.00},
-      { name: "Azeitona Preta", priceAdd: 4.00},
-      { name: "Azeite De Oliva", priceAdd: 5.00},
-      { name: "Orégano", priceAdd: 6.00}
+      { name: "Calabresa defumada", priceAdd: 3.00},
+      { name: " Cebola", priceAdd: 3.00},
+      { name: " Azeitona preta", priceAdd: 4.00},
+      { name: " Azeite de oliva", priceAdd: 5.00},
+      { name: " Orégano", priceAdd: 6.00}
     ],
     descri:
       "Uma pizza simples, mas que conquistou uma legião de fãs em todo o mundo. Está presente em mais de 30% dos pedidos.",
@@ -37,12 +37,12 @@ let pizzaJson = [
     img: "../../Mídia/Imagens/pizza3.png",
     price: 35.2,
     remAdd: [
-      { name: "Molho De Tomate", priceAdd: 3.00},
-      { name: "Mussarela", priceAdd: 3.00},
-      { name: "Gorgonzola", priceAdd: 4.00},
-      { name: "Parmesão", priceAdd: 5.00},
-      { name: "Provolone", priceAdd: 8.00},
-      { name: "Orégano", priceAdd: 8.00}
+      { name: "Molho de tomate", priceAdd: 3.00},
+      { name: " Mussarela", priceAdd: 3.00},
+      { name: " Gorgonzola", priceAdd: 4.00},
+      { name: " Parmesão", priceAdd: 5.00},
+      { name: " Provolone", priceAdd: 8.00},
+      { name: " Orégano", priceAdd: 8.00}
     ],
     descri:
       "Deliciosa e cremosa feita com farinha de trigo, fermento em pó, molho de tomate, muçarela, provolone, gorgonzola e parmesão ralado.",
@@ -56,15 +56,15 @@ let pizzaJson = [
     price: 38.0,
     remAdd: [
       { name: "Mussarela", priceAdd: 3.00},
-      { name: "Presunto", priceAdd: 3.00},
-      { name: "Tomate", priceAdd: 3.00},
-      { name: "Cebola", priceAdd: 5.00},
-      { name: "Alho", priceAdd: 5.00},
-      { name: "Azeite", priceAdd: 8.00},
-      { name: "Orégano", priceAdd: 8.00},
-      { name: "Ervilha", priceAdd: 9.00},
-      { name: "Ovos Cozidos", priceAdd: 9.00},
-      { name: "Azeitonas", priceAdd: 10.00}
+      { name: " Presunto", priceAdd: 3.00},
+      { name: " Tomate", priceAdd: 3.00},
+      { name: " Cebola", priceAdd: 5.00},
+      { name: " Alho", priceAdd: 5.00},
+      { name: " Azeite", priceAdd: 8.00},
+      { name: " Orégano", priceAdd: 8.00},
+      { name: " Ervilha", priceAdd: 9.00},
+      { name: " Ovos cozidos", priceAdd: 9.00},
+      { name: " Azeitonas", priceAdd: 10.00}
     ],
     descri:
       "Ovos, cebola, azeitona, ervilha, queijo e presunto dão sabor a segunda pizza mais queridinha dos brasileiros.",
@@ -78,9 +78,9 @@ let pizzaJson = [
     price: 35.5,
     remAdd: [
       { name: "Tomate", priceAdd: 3.00},
-      { name: "Azeite De Oliva", priceAdd: 3.00},
-      { name: "Queijo Mozzarella", priceAdd: 5.00},
-      { name: "Manjericão", priceAdd: 8.00}
+      { name: " Azeite de oliva", priceAdd: 3.00},
+      { name: " Mussarela", priceAdd: 5.00},
+      { name: " Manjericão", priceAdd: 8.00}
     ],
     descri:
       "Sabor ícone da Itália que leva apenas molho, muçarela, tomate e manjericão. É uma ótima opção para quem não come carne.",
@@ -92,12 +92,12 @@ let pizzaJson = [
     img: "../../Mídia/Imagens/pizza6.png",
     price: 34.0,
     remAdd: [
-      { name: "Peito De Frango", priceAdd: 3.00},
-      { name: "Orégano", priceAdd: 3.00},
-      { name: "Queijo Catupiry", priceAdd: 5.00},
-      { name: "Molho De Tomate", priceAdd: 5.00},
-      { name: "Pimenta", priceAdd: 8.00},
-      { name: "Salsinha", priceAdd: 8.00}
+      { name: "Peito de frango", priceAdd: 3.00},
+      { name: " Orégano", priceAdd: 3.00},
+      { name: " Catupiry", priceAdd: 5.00},
+      { name: " Molho de tomate", priceAdd: 5.00},
+      { name: " Pimenta", priceAdd: 8.00},
+      { name: " Salsinha", priceAdd: 8.00}
     ],
     descri: "A mistura de proteína e cremosidade que deu muito certo.",
     ingredi:
@@ -109,10 +109,10 @@ let pizzaJson = [
     img: "../../Mídia/Imagens/pizza7.png",
     price: 40.0,
     remAdd: [
-      { name: "Chocolate Meio Amargo", priceAdd: 3.00},
-      { name: "Manteiga", priceAdd: 5.00},
-      { name: "Leite Condensado", priceAdd: 8.00},
-      { name: "Granulado", priceAdd: 9.00}
+      { name: "Chocolate meio amargo", priceAdd: 3.00},
+      { name: " Manteiga", priceAdd: 5.00},
+      { name: " Leite condensado", priceAdd: 8.00},
+      { name: " Granulado", priceAdd: 9.00}
     ],
     descri:
       "O clássico doce brasileiro que cai bem de qualquer jeito e a qualquer hora. Faz o maior sucesso entre a criançada.",
